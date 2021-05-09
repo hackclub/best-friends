0bd75f989226ba36afd8e0646571b951bfedc011
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-09 13:10:18 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,22 @@ import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 import ColorSwitcher from '../components/color-switcher'
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Meta
+          as={Head}
+          name="Hack Club" // site name
+          title="Best Friends" // page title
+          description="Find out your best friend based on #poll-of-the-day results." // page description
+          image="https://cloud-fjdua2ero-hack-club-bot.vercel.app/0best_friendsss.png" // large summary card image URL
+          color="#ec3750" // theme color
+        />
         <ColorSwitcher />
         <Component {...pageProps} />
       </ThemeProvider>
